c0a98a6ea34d3225f61f687f703df6097bc1287c
alex-avila <alex.avilx@icloud.com>
2018-06-26 16:03:57 -0600

    Add index.css to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,15 @@ class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <h1>Hoi</h1>
-        <Switch>
+        {/* <h1>Hoi</h1> */}
+        {/* <Switch>
           <Route exact path='/' Component={home} />
           <Route path='/TickTacToe' Component={TickTacToe} />
           <Route path='/CardMatch' Component={MemoryGame} />
           <Route path='/ColorGuess' Component={ColorGuess} />
-        </Switch>
+        </Switch> */}
         <MemoryGame />
-        <Foot />
+        {/* <Foot /> */}
       </div>
     );
   }
