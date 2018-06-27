02ea67f890792f2afb31c37d3f06baede108f400
alex-avila <alex.avilx@icloud.com>
2018-06-26 19:16:10 -0600

    Reorganized files, made a simple home page, made a button for the in-game navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link, withRouter } from "react-router-dom";
 
-import MemoryGame from './components/MemoryGame'
-import TicTacToe from './components/TicTacToe/Game'
+import Home from './scenes/Home'
+import MemoryGame from './scenes/MemoryGame'
+import TicTacToe from './scenes/TicTacToe/Game'
+import BackButton from './components/BackButton';
 
 class App extends Component {
   render() {
+    console.log(this.props)
     return (
       <div className="wrapper">
-        {/* <h1>Hoi</h1> */}
-        {/* <Switch>
-          <Route exact path='/' Component={home} />
-          <Route path='/TickTacToe' Component={TickTacToe} />
-          <Route path='/CardMatch' Component={MemoryGame} />
-          <Route path='/ColorGuess' Component={ColorGuess} />
-        </Switch> */}
-        {/* <MemoryGame /> */}
-        <TicTacToe />
-        {/* <Foot /> */}
+        {
+          this.props.location.pathname !== '/' &&
+          <Link to="/">
+            <BackButton />
+          </Link>
+        }
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/TicTacToe' component={TicTacToe} />
+          <Route path='/cardMatch' component={MemoryGame} />
+          {/* <Route path='/ColorGuess' Component={ColorGuess} /> */}
+        </Switch>
       </div>
     );
   }
 }
 
-export default App
+export default withRouter(App)
