665bf9faa15a454093a0a4ec99140ed755f3b770
alex-avila <alex.avilx@icloud.com>
2018-06-28 19:28:47 -0600

    Add icon to preview GameList layout with icons

--- client/src/scenes/Home/components/GamesList/components/GameItem/index.js
+++ client/src/scenes/Home/components/GamesList/components/GameItem/index.js
@@ -2,22 +2,35 @@ import React, { Component } from 'react'
 
 import { Link } from 'react-router-dom'
 
+import Button from '../../../../../../components/Button'
+
+import ticTacToeIcon from '../../../../icons/tictactoe.svg'
+
 import './index.css'
 
 class GameLink extends Component {
     render() {
+        const iconStyle = {
+            background: `url(${ticTacToeIcon})`,
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
+            height: 60,
+            width: 60,
+            display: 'inline-block',
+            borderRadius: 10
+        }
         const { name, url } = this.props
         return (
             <div className="games__item">
                 <Link to={url} className="item__info">
-                    {/* <span className="item__info__icon"></span> */}
+                    <span style={iconStyle}></span>
                     <span className="item__info__title">{name}</span>
                 </Link>
                 <Link to={`${url}/play`}>
-                    <button>Play</button>
+                    <Button>Play</Button>
                 </Link>
             </div>
-        );
+        )
     }
 }
 
