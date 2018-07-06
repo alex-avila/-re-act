98ce320d05b64afe7904a0c1c239d373de5d38a8
tomg308 <tgutierrez308@gmail.com>
2018-07-05 22:24:12 -0600

    Add hp to rps

--- client/src/scenes/RockPaperScissors/RPS.js
+++ client/src/scenes/RockPaperScissors/RPS.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
-
 import { connect } from "react-redux";
 import { updateScores } from "../../redux/reducers/gamesReducer";
-
 import "./index.css";
 
 // import Hero from './charComponents/Hero';
@@ -17,7 +15,14 @@ class RPS extends Component {
       time: 3,
       timer: null,
       nullSwitch: false,
-      attackAnimation: ""
+      attackAnimation: "",
+      p1hp: 100,
+      p2hp: 100,
+      crit: 20,
+      weak: 0,
+      p1win: false,
+      p2win: false,
+      count: 0
     };
     this.initialState = this.state;
   }
@@ -41,26 +46,29 @@ class RPS extends Component {
     });
   };
 
-  handleGameFunction = bye => {
-    if (bye === "bye") {
-      this.setState(this.initialState);
+  handleGameFunction = () => {
+    if (this.state.nullSwitch) {
+      this.setState({ count: 0 });
+      this.setState({ time: 3 });
       clearInterval(this.state.timer);
-    } else {
-      const rng12 = Math.floor(Math.random() * 3 + 1);
-      if (rng12 === 1) {
-        this.setState({ playerTwo: "Fire" });
-      } else if (rng12 === 2) {
-        this.setState({ playerTwo: "Water" });
-      } else if (rng12 === 3) {
-        this.setState({ playerTwo: "Earth" });
+      if (!this.state.p1hp || !this.state.p2hp) {
+        this.setState(this.initialState);
       }
-
-      this.setState(prevState => {
-        return {
-          nullSwitch: !prevState.nullSwitch
-        };
-      });
     }
+    const rng12 = Math.floor(Math.random() * 3 + 1);
+    if (rng12 === 1) {
+      this.setState({ playerTwo: "Fire" });
+    } else if (rng12 === 2) {
+      this.setState({ playerTwo: "Water" });
+    } else if (rng12 === 3) {
+      this.setState({ playerTwo: "Earth" });
+    }
+
+    this.setState(prevState => {
+      return {
+        nullSwitch: !prevState.nullSwitch
+      };
+    });
   };
 
   handlePlayerCompare = x => {
@@ -71,27 +79,70 @@ class RPS extends Component {
       }
       this.props.updateScores(url, score);
     };
-    this.setState({ playerOne: x });
-    if (
-      (x === "Fire" && this.state.playerTwo === "Earth") ||
-      (x === "Earth" && this.state.playerTwo === "Water") ||
-      (x === "Water" && this.state.playerTwo === "Fire")
-    ) {
-      // go to redux and backend and update scores
-      updateScores(url, 1000);
-      console.log("Player One wins! ^.^");
-      this.setState({ results: "Player one wins! ^.^" });
-    } else if (x === this.state.playerTwo) {
-      // go to redux and backend and update scores
-      updateScores(url, 250);
-      console.log(`It's a draw`);
-      this.setState({ results: `It's a draw. -.-` });
-    } else {
-      // go to redux and backend and update scores
-      updateScores(url, 500);
-      console.log("Player Two Wins! ^.^");
-      this.setState({ results: "Player two wins! ^.^" });
+    //tom thing
+    if (this.state.nullSwitch && this.state.count === 0) {
+      this.setState({ count: 1 });
+      this.setState({ playerOne: x });
+      if (
+        (x === "Fire" && this.state.playerTwo === "Earth") ||
+        (x === "Earth" && this.state.playerTwo === "Water") ||
+        (x === "Water" && this.state.playerTwo === "Fire")
+      ) {
+        console.log("Player One wins! ^.^");
+        this.setState(
+          prevState => ({ p2hp: prevState.p2hp - prevState.crit }),
+          () => {
+            this.setState({
+              results: `Player one wins! ^.^\nPlayer 2 HP: ${this.state.p2hp}`
+            });
+            if (this.state.p1hp === 0 || this.state.p2hp === 0) {
+              console.log("oehowiehgoiwhegoiwhegoih");
+              if (this.state.p1hp === 0) {
+                // go to redux and backend and update scores
+                updateScores(url, 500);
+                this.setState({
+                  results: "You died"
+                });
+              } else {
+                // go to redux and backend and update scores
+                updateScores(url, 1000);
+                this.setState({
+                  results: `You have defeted the lord Tony \n yay!`
+                });
+              }
+            }
+          }
+        );
+      } else if (x === this.state.playerTwo) {
+        console.log(`It's a draw`);
+        this.setState({ results: `It's a draw. -.-` });
+      } else {
+        this.setState(
+          prevState => ({ p1hp: prevState.p1hp - prevState.crit }),
+          () => {
+            this.setState({
+              results: `Player two wins! ^.^\nPlayer 1 HP: ${this.state.p1hp}`
+            });
+            if (this.state.p1hp === 0 || this.state.p2hp === 0) {
+              if (!this.state.p1hp) {
+                // go to redux and backend and update scores
+                updateScores(url, 1000);
+                this.setState({
+                  results: "You died"
+                });
+              } else {
+                // go to redux and backend and update scores
+                updateScores(url, 500);
+                this.setState({
+                  results: `You have defeted the lord Tom mcLordinessFace \n yay!`
+                });
+              }
+            }
+          }
+        );
+      }
     }
+    //handle hp
   };
 
   render() {
@@ -100,16 +151,22 @@ class RPS extends Component {
       return player === "Fire" ? "red" : player === "Water" ? "blue" : "green";
     };
 
-      if(this.state.nullSwitch) {
-        this.props.handleAtkAni(true)
-      } else {
-        this.props.handleAtkAni(false)
-      }
+    if (this.state.nullSwitch) {
+      this.props.handleAtkAni(true);
+    } else {
+      this.props.handleAtkAni(false);
+    }
 
     return (
       <div>
         {this.state.nullSwitch ? (
           <div>
+            <div className="progress-bar">
+              <div className="progress" style={{ width: this.state.p1hp }} />
+            </div>
+            <div className="progress-bar-2">
+              <div className="progress-2" style={{ width: this.state.p2hp }} />
+            </div>
             <div className="timer">{this.state.time}</div>
             <div className="compare-buttons">
               <button onClick={() => this.handlePlayerCompare("Fire")}>
@@ -124,18 +181,20 @@ class RPS extends Component {
             </div>
           </div>
         ) : null}
+
         <button
           onClick={() => {
             if (!this.state.nullSwitch) {
               this.handleGameFunction();
               this.handleTimer();
             } else {
-              this.handleGameFunction("bye");
+              this.handleGameFunction();
             }
           }}
         >
           {this.state.nullSwitch ? "Reset" : "Start"}
         </button>
+
         {this.state.time === 0 && (
           <div
             style={
@@ -144,8 +203,8 @@ class RPS extends Component {
                 : null
             }
           >
-            <div className='resultStyles'>
-             <div className={getChoiceClass(playerOne)}>
+            <div className="resultStyles">
+              <div className={getChoiceClass(playerOne)}>
                 Player One: {this.state.playerOne}
               </div>
               <div className={getChoiceClass(playerTwo)}>
